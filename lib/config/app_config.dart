ca0a852e861245bc11c07f77a97f48a7903484ba
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-30 04:22:06 +0530

    m

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -23,9 +23,9 @@ class AppConfig {
   static String stripeCurrency = "USD".toUpperCase();
   static const String stripeMerchantID = "merchant.thegreatestmarkeplace";
   static const String stripePublishableKey =
-      "stripePublishableKey";
+      "pk_test_51OTydVHHgGZ1rB2oCwA3d4UjhW1ajimcjqR65FczIBSZqyYtaGl58N5zHwXxuC6w39UjAQmSBLXmoUoe9CZuxnoP00VWEgjtvZ";
   static const String stripeToken =
-      'stripeToken';
+      'sk_test_51OTydVHHgGZ1rB2oIpSFP0VPpk92x5vXBC30rGfbjITnq3IfjSYZRqOQ78sqTqEX7opbWgqxGxQkPOWbIjUbmBtL00kbanmzce';
   // static const String appPackageName = "com.infix.lms";
 
   ///
@@ -35,9 +35,9 @@ class AppConfig {
       "https://api.sandbox.paypal.com"; // "https://api.paypal.com"; // for production mode
   static const String paypalCurrency = 'USD';
   static const String paypalClientId =
-      'paypalClientId';
+      'AQgAWV4PlM9g81xZ51TLtVi68KjB89s4mpcchFschs7OvTM-3p4zsQTDqHOkv5Sw44k9goHlE-VAC7zj';
   static const String paypalClientSecret =
-      'paypalClientSecret';
+      'ELLoQfnZ4kRbDkul81U_RNRsgHgFPDumlUloCcX6nO6ziXRXKob8gVYaTn6CGCeNVJtBqsfv7VtbsuR2';
   static const bool sandboxMood = true;
 
   ///
@@ -46,7 +46,7 @@ class AppConfig {
   static const String payStackPublicKey =
       "pk_test_d85ff3d14475233d9da27df301185459f5d148b0";
   static const String payStackSecretKey =
-      "payStackSecretKey";
+      "sk_test_ee55a9b27d06843d868851a81362018d1aa9ff90";
   static const PaystackCurrency payStackCurrency = PaystackCurrency.zar;
   // static const String payStackCurrency = 'ZAR';
 }
